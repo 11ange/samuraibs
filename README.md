081aedc029903908615bd5f0f52ebb819f9a6191
11ange <luis.lange@gmail.com>
2022-07-08 18:40:25 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Samurai Barber Shop
 Curso Universo Cypress by QAcademy
+
+instalar pg para ítem 6 (https://node-postgres.com/features/pooling):
+npm install pg --save-dev
--- testes/cypress/e2e/signupPG.cy.js
+++ testes/cypress/e2e/signupPG.cy.js
@@ -1,41 +1,43 @@
 
-it('Deve cadastrar um novo usuário', function(){
-    const nome = 'Nada'
-    const email = 'nada@nda.com'
-    const senha = 'pwd123'
+describe('cadastro', function () {
+    it('Deve cadastrar um novo usuário', function () {
+        const nome = 'Nada'
+        const email = 'nada@nda.com'
+        const senha = 'pwd123'
 
-    cy.task('removeUser', email)
-        .then(function(result){
-            console.log(result)
-        })
+        cy.task('removeUser', email)
+            .then(function (result) {
+                console.log(result)
+            })
 
-    cy.visit('/signup')
-    cy.get('input[placeholder="Nome"]').type(nome)
-    cy.get('input[placeholder="E-mail"]').type(email)
-    cy.get('input[placeholder="Senha"]').type(senha)
+        cy.visit('/signup')
+        cy.get('input[placeholder="Nome"]').type(nome)
+        cy.get('input[placeholder="E-mail"]').type(email)
+        cy.get('input[placeholder="Senha"]').type(senha)
 
-    cy.contains('button','Cadastrar').click()
+        cy.contains('button', 'Cadastrar').click()
 
-    cy.get('.toast')
-        .should('be.visible')
-        .find('p')
-        .should('have.text','Agora você pode fazer seu login no Samurai Barbershop!')
-})
+        cy.get('.toast')
+            .should('be.visible')
+            .find('p')
+            .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+    })
 
-it('Deve exibir email já cadastrado', function(){
-    const nome = 'Nada'
-    const email = 'nada@nda.com'
-    const senha = 'pwd123'
+    it('Deve exibir mensagem de email já cadastrado', function () {
+        const nome = 'Nada'
+        const email = 'nada@nda.com'
+        const senha = 'pwd123'
 
-    cy.visit('/signup')
-    cy.get('input[placeholder="Nome"]').type(nome)
-    cy.get('input[placeholder="E-mail"]').type(email)
-    cy.get('input[placeholder="Senha"]').type(senha)
+        cy.visit('/signup')
+        cy.get('input[placeholder="Nome"]').type(nome)
+        cy.get('input[placeholder="E-mail"]').type(email)
+        cy.get('input[placeholder="Senha"]').type(senha)
 
-    cy.contains('button','Cadastrar').click()
+        cy.contains('button', 'Cadastrar').click()
 
-    cy.get('.toast')
-        .should('be.visible')
-        .find('p')
-        .should('have.text','Email já cadastrado para outro usuário.')
+        cy.get('.toast')
+            .should('be.visible')
+            .find('p')
+            .should('have.text', 'Email já cadastrado para outro usuário.')
+    })
 })
